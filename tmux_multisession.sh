dbf0c29facc87d6d01f890892e6ba8bf5e2a83de
harneet85 <31202606+harneet85@users.noreply.github.com>
2020-06-26 12:51:38 +0530

    Changed to functions
    Changed to functions

--- tmux_multisession.sh
+++ tmux_multisession.sh
@@ -1,63 +1,84 @@
 #!/bin/bash
-# toxsocks ssh -l in0090g5-multi
-# D.Kovalov
-# Based on http://linuxpixies.blogspot.jp/2011/06/tmux-copy-mode-and-how-to-control.html
+########################################################
+#########################################################
+## CREATED BY HARNEET SINGH 
+## DATE - 26 JUNE 20202
+## Purpose - To run commands on multiple host 
+##          in a single session , multiple panes window.
+########################################################
 
-# a script to toxsocks ssh -l in0090g5 multiple servers over multiple tmux panes
+###########################
+#### Variables ############
+##########################
 
 command="toxsocks /home/harneet/PycharmProjects/tsb1/venv/bin/python /home/harneet/PycharmProjects/tsb1/main.py"
 #rcommand=top
+carg="$#"
+aarg=( $@ )
 
 
+################ Welcome function #######################
 
-if (( "$#" < "1" )) ; then
-echo "
---------Error-------------
-Minimum one host required 
---------------------------
-Usage -
-	script host1 host2 host3 ......
-"
-exit 1
-fi
+function welcome() {
+	printf "\n\t\t###############################"
+	printf "\n\t\t## CREATED BY HARNEET SINGH ##"
+	printf "\n\t\t###############################\n\n"
+}
 
+################ check argument function #############
 
-if [ -z "$rcommand" ]; then
-	echo "Require command to execute on remote servers: [ENTER] "
-	read rcommand
+checkArg() {
+	if (( "$carg" < "1" )) ; then
+		echo "
+		--------Error-------------
+		Minimum one host required 
+		--------------------------
+		Usage -
+			script host1 host2 host3 ......
+		"
+	exit 1
+	fi
+}
+
+############## Pass command to execute remotely ########
+
+remoteCommand() {
+	if [ -z "$rcommand" ]; then
+		echo "Require command to execute on remote servers: [ENTER] "
+		read rcommand
 	
-fi
+	fi
+}
 
 
-hosts=( $@ )
+################ Main function ######################
+
 starttmux() {
 
-count=${#hosts[@]}
-echo $count
-echo ${hosts[@]}
-
+hosts=( ${aarg[@]} )
+count=$carg
 
 tmux new -s harneet -d "$command ${hosts[0]} \" $rcommand \"; bash"
-#tmux new -s harneet -d "alias;tt ${hosts[0]}; bash"
-#tmux new -s harneet -d 
-#tmux send-keys -t "harneet:0" "tt ${hosts[0]}" enter
 hosts=("${hosts[@]:1}")
     
 for i in "${hosts[@]}"; do
 	if (( $count % 2 )) ; then
 		tmux split-window -dv "$command $i \" $rcommand \"; bash"
-		#z=$((100/$count));tmux split-window -dv -p $z "$command $i \" $rcommand \"; bash"
-		#z=$((100/$count));tmux split-window -dh -p $z ; tmux send-keys "tt $i" enter
 	else
 		tmux split-window -dh "$command $i \" $rcommand \"; bash" 
-		#z=$((100/$count));tmux split-window -dh -p $z "$command $i \" $rcommand \"; bash" 
 	fi
 	count=$((count-1))
     done
 tmux select-layout tiled
 }
 
-HOSTS=${HOSTS:=$*}
+############# Start execution #####################
 
+welcome
+checkArg
+remoteCommand
 starttmux
 tmux attach
+
+######## SCRIPT END #############################
+
